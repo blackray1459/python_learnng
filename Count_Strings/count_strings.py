42717442cccdefe766cae5a63433f3f2751c4746
blackray1459 <kamarches@ya.ru>
2020-10-08 00:36:42 +0300

    Сделал программу, выравнивает по правому краю. Попробовать очистить код.

--- Count_Strings/count_strings.py
+++ Count_Strings/count_strings.py
@@ -4,7 +4,7 @@ from sortedcontainers import SortedDict
 
 d = defaultdict(int)
 length_key = 0
-for i in range(11):
+for i in range(15):
     key = str(input()).strip()
     d[key] += 1
     if len(key) > length_key:
@@ -15,3 +15,9 @@ length_val = 0
 for word in d.values():
     if len(str(word)) > length_val:
         length_val = len(str(word))
+
+print("+" + "-" * (2 + length_key) + "+" + "-" * (2 + length_val) + "+")
+for key, value in d.items():
+    print("| " + (lambda a: str(a) + " " * (length_key - key.__str__().__len__()))(key) + " | " + (lambda a: " " *
+                                                      (length_val - value.__str__().__len__()) +  str(a))(value) + " |")
+print("+" + "-" * (2 + length_key) + "+" + "-" * (2 + length_val) + "+")

048cc614212d50fd76bf7bda9bc6316c678521d1
blackray1459 <kamarches@ya.ru>
2020-10-08 17:26:53 +0300

    Завершено.

--- Count_Strings/count_strings.py
+++ Count_Strings/count_strings.py
@@ -1,28 +1,23 @@
+# ВВОД И СОРТИРОВКА СЛОВАРЯ С ПОДСЧЕТОМ ВВЕДЕННОЙ СТРОКИ
+
 from collections import defaultdict
-from sortedcontainers import SortedDict
-
-def sort_dict_by_value(d):
-    for key, value in d.items():
-
-    return d
-
+from sys import stdin
 
 counter = defaultdict(int)
 max_key_length = 0
-for i in range(15):
-    key = str(input()).strip()
+max_val_length = 0
+for key in stdin:
+    if key == '\n' or key == '':
+        break
+    key = str(key).strip()
     counter[key] += 1
     if len(key) > max_key_length:
         max_key_length = len(key)
-    #(lambda word, d: d[word]+=1)(word,d)
-d = dict(SortedDict(counter))
-max_val_length = 0
-for word in counter.values():
-    if len(str(word)) > max_val_length:
-        max_val_length = len(str(word))
+    if len(str(counter[key])) > max_val_length:
+        max_val_length = len(str(counter[key]))
 
 print("+" + "-" * (2 + max_key_length) + "+" + "-" * (2 + max_val_length) + "+")
-for key, value in d.items():
-    print("| " + (lambda a: str(a) + " " * (max_key_length - key.__str__().__len__()))(key) + " | " + (lambda a: " " *
-                                                      (max_val_length - value.__str__().__len__()) +  str(a))(value) + " |")
+for key, value in sorted(counter.items(), key=lambda x: x[1]):
+    print("| " + key + " " * (max_key_length - len(key)) + " | " + " " * (max_val_length - len(str(value))) +
+          str(value) + " |")
 print("+" + "-" * (2 + max_key_length) + "+" + "-" * (2 + max_val_length) + "+")

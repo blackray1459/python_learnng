84f2468073b3928226d1df02212269ebbe0019ab
blackray1459 <kamarches@ya.ru>
2020-10-08 18:15:11 +0300

    Завершено, добавлена работа с файлом

--- Count_Strings/count_strings.py
+++ Count_Strings/count_strings.py
@@ -3,21 +3,28 @@
 from collections import defaultdict
 from sys import stdin
 
-counter = defaultdict(int)
-max_key_length = 0
-max_val_length = 0
-for key in stdin:
-    if key == '\n' or key == '':
-        break
-    key = str(key).strip()
-    counter[key] += 1
-    if len(key) > max_key_length:
-        max_key_length = len(key)
-    if len(str(counter[key])) > max_val_length:
-        max_val_length = len(str(counter[key]))
 
-print("+" + "-" * (2 + max_key_length) + "+" + "-" * (2 + max_val_length) + "+")
-for key, value in sorted(counter.items(), key=lambda x: x[1]):
-    print("| " + key + " " * (max_key_length - len(key)) + " | " + " " * (max_val_length - len(str(value))) +
-          str(value) + " |")
-print("+" + "-" * (2 + max_key_length) + "+" + "-" * (2 + max_val_length) + "+")
+def file_to_dict_print_table(f):  # РАСПЕЧАТКА ТАБЛИЦЫ С ПОДСЧЕТОМ СТРОК ИЗ ФАЙЛА
+    counter = defaultdict(int)
+    max_key_length = 0
+    max_val_length = 0
+    for val in f:
+        if val == '\n' or val == '':
+            break
+        val = str(val).strip()
+        counter[val] += 1
+        if len(val) > max_key_length:
+            max_key_length = len(val)
+        if len(str(counter[val])) > max_val_length:
+            max_val_length = len(str(counter[val]))
+    print("+" + "-" * (2 + max_key_length) + "+" + "-" * (2 + max_val_length) + "+")
+    for val, count in sorted(counter.items(), key=lambda x: x[1]):
+        print("| " + val + " " * (max_key_length - len(val)) + " | " + " " * (max_val_length - len(str(count))) +
+              str(count) + " |")
+    print("+" + "-" * (2 + max_key_length) + "+" + "-" * (2 + max_val_length) + "+")
+    return dict(counter)
+
+
+with open('text.txt', 'r') as f:
+    file_to_dict_print_table(f)
+

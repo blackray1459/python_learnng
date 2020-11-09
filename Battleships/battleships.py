5d9feb242e704e7cd67a03cf52fda0087f86d652
blackray1459 <kamarches@ya.ru>
2020-11-09 22:42:59 +0300

    Доделал текстовый морской бой!

--- Battleships/battleships.py
+++ Battleships/battleships.py
@@ -13,6 +13,7 @@ import random
 dict_size_name = {4: "Aerocarrier", 3: "Cruiser", 2: "Destroyer", 1: "Boat"}
 letter_to_number = {'A': 0, 'B': 1, 'C': 2, 'D': 3, 'E': 4, 'F': 5, 'G': 6, 'H': 7, 'I': 8, 'J': 9}
 number_to_letter = {0: 'A', 1: 'B', 2: 'C', 3: 'D', 4: 'E', 5: 'F', 6: 'G', 7: 'H', 8: 'I', 9: 'J'}
+count = 0
 
 
 class Ship(object):
@@ -240,29 +241,80 @@ def place_ship(cell, direction, size):
 
 
 def shot(i, j):  # РЕГИСТРАЦИЯ ВЫСТРЕЛА И ПРОВЕРКА НА УБИЙСТВО
-    global shadow_field, battle_field
+    global shadow_field, battle_field, count
+    around = [-1, 0, 1]
+    counters = [[-1, 0], [1, 0], [0, -1], [0, 1]]  # ВВЕРХ, ВНИЗ, ВЛЕВО, ВПРАВО
     if shadow_field[i][j] == " " or shadow_field[i][j] == "+":
         battle_field[i][j] = "●"
+        shadow_field[i][j] = "●"
+        print("MIMO!")
         show_field()
+        return
+    elif shadow_field[i][j] == "●" or shadow_field[i][j] == "X":
+        print(f"Uzhe strelyal suda. Tut {shadow_field[i][j]}")
+        return
     elif shadow_field[i][j] == "◙":
         battle_field[i][j] = "X"
-
-
-
+        shadow_field[i][j] = "X"
+        for vertical_count, horizontal_count in counters:
+            m = vertical_count
+            n = horizontal_count
+            while True:
+                if 0 <= (i + m) <= 9 and 0 <= (j + n) <= 9:
+                    if shadow_field[i + m][j + n] == "◙":
+                        print("RANIL!")
+                        show_field()
+                        return
+                    elif shadow_field[i + m][j + n] == "+" or shadow_field[i + m][j + n] == "●":
+                        break
+                    else:
+                        m += vertical_count
+                        n += horizontal_count
+                else:
+                    break
+        #  ЕСЛИ НЕ НАШЛОСЬ ЗДОРОВОЙ ПАЛУБЫ
+        for vertical_count, horizontal_count in counters:
+            m = 0
+            n = 0
+            while True:
+                if (i + m) >= 0 and (j + n) >= 0 or (i + m) <= 9 and (j + n) <= 9:
+                    if shadow_field[i + m][j + n] == "X":
+                        for p in around:
+                            for r in around:
+                                try:
+                                    if (i + m + p < 0) or (j + n + r < 0):
+                                        continue
+                                    elif shadow_field[i + m + p][j + n + r] == "+":
+                                        shadow_field[i + m + p][j + n + r] = "●"
+                                        battle_field[i + m + p][j + n + r] = "●"
+                                except IndexError:
+                                    continue
+                        m += vertical_count
+                        n += horizontal_count
+                    elif shadow_field[i + m][j + n] == "+" or shadow_field[i + m][j + n] == "●":
+                        break
+                else:
+                    break
+        print("POTOPIL!")
+        show_field()
+        count += 1
 
 
 shadow_field = [[" "] * 10 for i in range(10)]
 battle_field = [[" "] * 10 for i in range(10)]
-print(number_to_letter.keys())
 
 random_place_all()
 turn = ""
 while turn != "stop":
     turn = input("\nEnter the cell you want to shoot: ")
+    print()
     try:
         if turn == "":
             print("Cell is not chosen.")
             continue
+        elif turn == "show":
+            show_field()
+            show_field("shadowfield")
         elif (type(turn) is str) and len(turn) == 2 and turn[0].isalpha() and turn[1].isdigit():
             if turn[0].upper() not in letter_to_number.keys():
                 print("Vvedi normalnuiy bukvu! A B C D E F G H I J. Vybirai!")
@@ -270,28 +322,17 @@ while turn != "stop":
             if int(turn[1]) not in number_to_letter.keys():
                 print("Cifry ot 0 do 9, blin! I bez zapyatyh!")
                 continue
-            print("Normalno vvel")
             i = int(turn[1])-1
             j = letter_to_number[turn[0].upper()]
-            # РЕГИСТРАЦИЯ ВЫСТРЕЛА И ПРОВЕРКА НА УБИЙСТВО
-            print(i, j)
-            shadow_field[i][j] = "#"
-            show_field("shadow_field")
+            shot(i, j)
             continue
         else:
             print("Nichego ne ponyal. Vvod takoi: A4, B7. Bukva i cifra. Davai po novoi, Misha!")
             continue
-
     except TypeError:
         print("Wrong input. Vvodi kak nado.")
         continue
-'''
-show_field(shadow_field)
-print()
-four_celled = Ship("Aerocarrier", 4)
-four_celled.place()
-show_field(shadow_field)
-three_celled_1 = Ship("Cruiser 1", 3)
-three_celled_1.place()
-show_field(shadow_field)
-'''
+    if count == 10:
+        print("Eto pobeda! Ty ih nashel!")
+        break
+
